17dd45fe0dcfaad89c4c76c5e04afb7fad660e7a
Jiaming Chen <jiamingchen05@gmail.com>
2017-04-03 11:57:02 -0400

    fixed comparator sig_in hex values, and minor syntax

--- NFA_script/seq_match.py
+++ NFA_script/seq_match.py
@@ -12,11 +12,10 @@ def write_header(fw, size):
     fw.write("\tport(\n")
     fw.write("\t\tclk: in std_logic;\n")
     fw.write("\t\treset: in std_logic;\n")
-    fw.write("\t\ten: in std_logic;\n")
     fw.write("\t\tdata_in: in std_logic_vector(7 downto 0);\n")
 
     for i in range (size-1):
-        fw.write("\t\toutput_" + str(i) + " : out std_logic);\n")
+        fw.write("\t\toutput_" + str(i) + " : out std_logic;\n")
 
     fw.write("\t\toutput_" + str(size-1) + " : out std_logic\n")
     fw.write("\t);\n")
@@ -59,7 +58,7 @@ def write_signals(fw, hex_list):
     for i in range(len(hex_list)):
         fw.write("-- " + hex_list[i].decode("hex") + "\n");
         for j in range(0, len(hex_list[i]), 2):
-            fw.write("signal cmp_s" + str(i) + '_0x' + hex_list[i][j:j+2] + '_' + str(j/2) + ': std_logic;')
+            fw.write("signal cmp_s" + str(i) + '_0x' + hex_list[i][j:j+2] + ': std_logic;')
             fw.write("\t--" + hex_list[i][j:j+2].decode("hex") +'\n')
             fw.write("signal s" + str(i) + "_ff_" + str(j/2) + ": std_logic := '0';\n")
             fw.write("signal out_and_s" + str(i) + "_ff_" + str(j/2) + ": std_logic := '0';\n\n")
@@ -75,7 +74,7 @@ def write_ports(fw, hex_list):
     for i in range(len(hex_list)):
         for j in range(0, len(hex_list[i]), 2):
             fw.write("\tbyte_comp_s"+ str(i) + '_' + hex_list[i][j:j+2] + '_' + str(j/2) +  ':')
-            fw.write(' byte_compare port map( data_in => input, sig_in => x"' + hex_list[i][j:j+2] + '_' + str(j/2) + '" ')
+            fw.write(' byte_compare port map( data_in => input, sig_in => x"' + hex_list[i][j:j+2] + '" ')
             fw.write(", is_valid => cmp_s" + str(i) + '_0x' + hex_list[i][j:j+2] + '_' + str(j/2)+ ");\n")
             
             if j == 0:
@@ -88,7 +87,7 @@ def write_ports(fw, hex_list):
             
    
             fw.write("\tand_s" + str(i) + "_ff_" + str(j/2) + ": out_and_s" + str(i) + "_ff_" + str(j/2) + " <= " + "s" + str(i) + "_ff_" + str(j/2) + " and")
-            fw.write(" cmp_s" + str(i) + '_0x' + hex_list[i][j:j+2] + '_' + str(j/2) +  ");\n");
+            fw.write(" cmp_s" + str(i) + '_0x' + hex_list[i][j:j+2] + '_' + str(j/2) +  ";\n");
         fw.write("output_" + str(i) + " <= out_and_s" + str(i) + "_ff_" + str(j/2)+ ';\n')
         fw.write('\n')   
 
