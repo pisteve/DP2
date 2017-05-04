8b85024cad6d38841b756f557c9b0c74e9602bd8
Jiaming Chen <jiamingchen05@gmail.com>
2017-05-04 16:10:01 -0400

    added pull_up module

---
+++ NFA_script/pull_up_module.vhd
@@ -0,0 +1,26 @@
+library IEEE
+use IEEE.STD_LOGIC_1164.all;
+
+entity pull_up_module is
+port (
+        clk: in std_logic;
+        reset: in std_logic;
+        isMatching: in std_logic;
+        out_isMatching: out std_logic
+	 );  
+end pull_up_module;
+
+architecture behavioral of pull_up_module is
+begin
+
+    process(clk, reset, isMatching)
+    begin  
+        if(reset = '1') then    
+            out_isMatching <= '0';
+        elsif (rising_edge(clk)) then
+            if (isMatching = '1') then
+                out_isMatching <= '1';
+            end if;
+        end if;
+    end process;
+end behavioral;
--- NFA_script/seq_match.py
+++ NFA_script/seq_match.py
@@ -1,8 +1,15 @@
 #!usr/bin/python
 import sys
 
+'''
 
-def write_header(fw, size):
+python seq_detect.py signatures.txt 
+only accepts 2 arguments
+outputs a seq_detect.vhd and pull_up.vhd
+
+'''
+
+def seq_detect_header(fw, size):
     fw.write("Library IEEE;\n")
     fw.write("USE IEEE.STD_LOGIC_1164.ALL;\n")
     fw.write("USE IEEE.STD_LOGIC_UNSIGNED.ALL;\n")
@@ -21,7 +28,7 @@ def write_header(fw, size):
     fw.write("\t);\n")
     fw.write("end detector;\n\n")
 
-def write_components(fw):
+def seq_detect_comps(fw):
     fw.write("component byte_compare")
     fw.write("\tport(\n")
     fw.write("\t\tclk :  std_logic;\n")
@@ -51,7 +58,7 @@ def hexify(sigList):
         list.append(hexVal)
     return list
 
-def write_signals(fw, hex_list):
+def seq_detect_signals(fw, hex_list):
 
     fw.write("signal input : std_logic_vector(7 downto 0);\n")
 
@@ -64,14 +71,10 @@ def write_signals(fw, hex_list):
             fw.write("signal s" + str(i) + "_ff_" + str(j/2) + ": std_logic := '0';\n")
             fw.write("signal out_and_s" + str(i) + "_ff_" + str(j/2) + ": std_logic := '0';\n\n")
         fw.write('\n')
-    
-    # for i in range(len(hex_list)):
-    #     for j in range(0, len(hex_list[i]), 2):
-    #         fw.write("signal: FF_s" + str(i) + '_f' + str(j/2) + '_' + hex_list[i][j:j+2] + ': std_logic : =0;\n')
-    #     fw.write('\n')
 
 
-def write_ports(fw, hex_list):
+
+def seq_detect_ports(fw, hex_list):
     for i in range(len(hex_list)):
         for j in range(0, len(hex_list[i]), 2):
             fw.write("\tbyte_comp_s"+ str(i) + '_' + hex_list[i][j:j+2] + '_' + str(j/2) +  ':')
@@ -93,23 +96,95 @@ def write_ports(fw, hex_list):
         fw.write('\n')   
 
 
-def write_architecture(fw, hex_list):
+def seq_detect_arch(fw, hex_list):
     fw.write("architecture behavioral of detector is\n\n")
     
-    write_components(fw)
-    write_signals(fw, hex_list)
+    seq_detect_comps(fw)
+    seq_detect_signals(fw, hex_list)
 
 
     fw.write("begin\n")
     fw.write("\tinput <= data_in;\n");
-    write_ports(fw, hex_list);
+    seq_detect_ports(fw, hex_list);
 
     fw.write("end behavioral;\n")
+
+
+def pull_up_header(fw, count):
+    fw.write("Library IEEE;\n")
+    fw.write("USE IEEE.STD_LOGIC_1164.ALL;\n")
+    fw.write("USE IEEE.STD_LOGIC_UNSIGNED.ALL;\n")
+    fw.write('\n')
+
+    fw.write("entity pull_up is\n")
+    fw.write("\tport(\n")
+    fw.write("\t\tclk: in std_logic;\n")
+    fw.write("\t\treset: in std_logic;\n")
+    for i in range (count-1):
+        fw.write("\t\tisMatching_" + str(i) + ": in std_logic;\n")
+    fw.write("\t\tisMatching_" + str(count-1) + ": in std_logic;\n")
+    for i in range (count-1):
+        fw.write("\t\tpull_up_" + str(i) + ": out std_logic;\n")
+    fw.write("\t\tpull_up_" + str(count-1) + " : out std_logic\n")
+    fw.write("\t);\n")
+    fw.write("end pull_up;\n\n")
+
+def pull_up_comps(fw):
+    fw.write("component pull_up_module")
+    fw.write("\tport(\n")
+    fw.write("\t\tclk :  std_logic;\n")
+    fw.write("\t\treset :  std_logic;\n")
+    fw.write("\t\tisMatching : in std_logic;\n")
+    fw.write("\t\tout_isMatching : out std_logic")
+    fw.write(");\n")
+    fw.write("end component;\n\n");
+
+
+    fw.write("component FF")
+    fw.write("\tport(\n")
+    fw.write("\t\tclk : in std_logic;\n")
+    fw.write("\t\treset : in std_logic;\n")
+    fw.write("\t\tinput : in std_logic;\n")
+    fw.write("\t\toutput : out std_logic\n")
+    fw.write(");\n")
+    fw.write("end component;\n\n")
+
+def pull_up_signals(fw, count):
+    for i in range(count):
+        fw.write("signal hold_" + str(i) + ": std_logic;\n")
+    for i in range(count):
+        fw.write("signal release_" + str(i) + ": std_logic;\n")
+
+def pull_up_port(fw, count):
+    for i in range(count):
+         fw.write("\tpull_" + str(i) + ": pull_up_module")
+         fw.write(" port map(clk => clk, reset => reset, isMatching => isMatching_" + str(i) + " out_isMatching => hold_" + str(i) + ");\n")
+    
+    for i in range(count):
+         fw.write("\tFF_out_" + str(i) + ": FF")
+         fw.write(" port map(clk => clk, reset => reset, input => release_" + str(i) + " output => pull_up_" + str(i) + ");\n")
+    for i in range(count):
+         fw.write("\trelease_" + str(i) + " <= hold_" + str(i) + ";\n")
+
+
+
+def pull_up_arch(fw, hex_list):
+    fw.write("architecture behavioral of pull_up is\n\n")
+    
+    pull_up_comps(fw)
+    pull_up_signals(fw, len(hex_list))
+    fw.write("begin\n")
+    
+    pull_up_port(fw, len(hex_list))
+
+    fw.write("end behavioral;\n")
+
+
 def main():
 
-    if(len(sys.argv)!=3):
-        print "error, script only accepts 3 arguments"
-        print "example: python seq_match.py inputFile.txt outputFile.vhd"
+    if(len(sys.argv)!=2):
+        print "error, script only accepts 2 arguments"
+        print "example: python seq_match.py inputFile.txt outputFile"
         sys.exit()
 
     sig_list = list()
@@ -127,10 +202,15 @@ def main():
             
         sig_list.sort()
     
-    fw = open(sys.argv[2]+'.vhd', 'w')
-    write_header(fw, len(sig_list))
+    fw = open('seq_detect.vhd', 'w')
+    seq_detect_header(fw, len(sig_list))
     hex_list = hexify(sig_list);
-    write_architecture(fw, hex_list)
+    seq_detect_arch(fw, hex_list)
+    fw.close();
+
+    fw = open('pull_up.vhd', 'w')
+    pull_up_header(fw, len(hex_list))
+    pull_up_arch(fw, hex_list);
 
 
 if __name__ == '__main__':
